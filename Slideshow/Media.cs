e313804325f2d942e7673dc0f977c19b4e1bca0b
Andrei Nicholson <contact@andreinicholson.com>
2013-08-09 22:30:31 -0400

    Initia commit

---
+++ Slideshow/Media.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Slideshow
+{
+    class Media
+    {
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public string FilePath { get; set; }
+    }
+}
---
+++ Slideshow/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Slideshow")]
+[assembly: AssemblyDescription("Loads all images in directory into slideshow")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andrei Nicholson")]
+[assembly: AssemblyProduct("Slideshow")]
+[assembly: AssemblyCopyright("Copyright © Andrei Nicholson 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4edf14bd-e51c-49f6-9fb4-5e63d322dee0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
