9ac4985ed45f32244af6b9064479f1989f944eb5
Andrei Nicholson <contact@andreinicholson.com>
2014-01-04 12:29:38 -0500

    Bump version to 1.0.1

--- Slideshow/Properties/AssemblyInfo.cs
+++ Slideshow/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -12,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Andrei Nicholson")]
 [assembly: AssemblyProduct("Slideshow")]
-[assembly: AssemblyCopyright("Copyright © Andrei Nicholson 2013")]
+[assembly: AssemblyCopyright("Copyright © Andrei Nicholson 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,4 +49,4 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
