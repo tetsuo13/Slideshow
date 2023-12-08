21b2c88bbd4caea4d1309df4c74e9f835220a5d0
Michael Hasling <michael.hasling@gmail.com>
2023-12-08 11:32:11 +0000

    Add support for drag'n'drop onto the application in file explorer. A small change to the duration decrement feature to only wqhalve the duration at faster speeds.

--- Slideshow/App.xaml.cs
+++ Slideshow/App.xaml.cs
@@ -23,5 +23,18 @@ namespace Slideshow
     /// </summary>
     public partial class App : Application
     {
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length > 0)
+            {
+                dragNdropArg = e.Args[0];
+            }
+            else
+            {
+                dragNdropArg = "";
+            }
+        }
+
+        public static string dragNdropArg;
     }
 }
